1cfd6f925d23aad9bc000c5a4f6fa78e641f6a53
khanhlam333 <khanhthlam@gmail.com>
2023-12-08 17:11:48 +0700

    Add project files.

---
+++ TheSecretGarden/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheSecretGarden.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Fiction()
+        {
+            return View();
+        }
+
+        public IActionResult NonFiction()
+        {
+            return View();
+        }
+
+        public IActionResult LimitedEdition()
+        {
+            return View();
+        }
+    }
+}
