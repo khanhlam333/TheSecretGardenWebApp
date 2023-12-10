0189ecf03650f60d2717695c74f5810d2d095170
khanhlam333 <khanhthlam@gmail.com>
2023-12-10 14:55:10 +0700

    add admin basic layout

---
+++ TheSecretGarden/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheSecretGarden.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BooksManage()
+        {
+            return View();
+        }
+
+        public IActionResult OrdersManage()
+        {
+            return View();
+        }
+
+        public IActionResult CustomersManage()
+        {
+            return View();
+        }
+    }
+}
